584d5bb344d1c0f2ec95974e5aa404f1f55ca66e
Kadievka <kadievka.s.ks@gmail.com>
2020-05-12 09:25:38 -0400

    Rutas y Componentes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import Home from './pages/Home';
-import Login from './pages/Login';
 import  './App.css';
-
-import {
-  BrowserRouter as Router,
-  Link,
-  Route
-} from 'react-router-dom';
 
 class App extends Component {
 
@@ -22,16 +14,7 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-
-        <Router>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/" style={{"marginLeft": "1em"}}>Home</Link>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/login" component={Login}></Route>
-          </div>
-        </Router>
-
+        {this.props.children}
       </MuiThemeProvider>
     );
   }
---
+++ src/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import App from './App';
+
+import {
+  BrowserRouter as ReactRouter,
+  Route
+} from 'react-router-dom';
+
+export default class Router extends React.Component{
+  render(){
+    return(
+      <ReactRouter>
+        <App><Route exact path="/" component={Home}></Route>
+          <Route exact path="/login" component={Login}></Route>
+        </App>
+      </ReactRouter>
+    )
+  }
+}
