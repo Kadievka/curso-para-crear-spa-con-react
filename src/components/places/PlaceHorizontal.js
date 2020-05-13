eaf8df54e08731ebd9f8d8b4ef32b098ee4a5219
Kadievka <kadievka.s.ks@gmail.com>
2020-05-13 12:53:34 -0400

    Dashboard para usuarios

---
+++ src/components/places/PlaceHorizontal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class PlaceHorizontal extends React.Component{
+  
+  render(){
+    return(
+      <h1 style={{"textAlign": "left"}}>{this.props.place.title}</h1>
+    );
+  }
+}
