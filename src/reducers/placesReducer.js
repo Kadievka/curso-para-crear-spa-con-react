82e1300f67eee79430132e5792e5945f8c152b24
Kadievka <kadievka.s.ks@gmail.com>
2020-05-23 13:32:45 -0400

    Crear acciones

--- src/reducers/placesReducer.js
+++ src/reducers/placesReducer.js
@@ -1,3 +1,9 @@
 export default function placesReducer(state = [], action){//todos reciben el state y una acción
+  switch(action.type){
+    case 'LOAD_PLACES':
+      return action.places;
+    default:
+      return state;
+  }
   return state;
 }
